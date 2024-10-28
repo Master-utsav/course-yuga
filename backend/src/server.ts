519fad8b7e88bdfa153914eeeedf366bc5478d09
master-utsav <utsavjaiswal890@gmail.com>
2024-10-28 10:58:07 +0530

    deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,6 @@ import connectDB from "./utils/db.config";
 import userRoute from "./routes/user.route";
 import courseRoute from "./routes/course.route";
 import videoRoute from "./routes/video.route";
-import { VercelRequest, VercelResponse } from "@vercel/node";
 
 // Initialize environment variables
 dotenv.config();
@@ -29,6 +28,4 @@ app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/video", videoRoute);
 
 // Export as Vercel-compatible handler
-export default (req: VercelRequest, res: VercelResponse) => {
-  app(req, res); // Forward request to Express app
-};
+export default app;
