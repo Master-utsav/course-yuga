b23af466420f01f50080ce728d5d84ffff4e20a6
master-utsav <utsavjaiswal890@gmail.com>
2024-10-28 11:43:51 +0530

    deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,46 @@
-import express, {Response , Request} from "express";
+import express, { Response, Request } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./utils/db.config";
 import userRoute from "./routes/user.route";
 import courseRoute from "./routes/course.route";
 import videoRoute from "./routes/video.route";
-import { VercelRequest, VercelResponse } from "@vercel/node";
 
 dotenv.config();
 const app = express();
-app.use(cors());
-app.use(express.json());
 const PORT = process.env.PORT || 8001;
 
-connectDB();
+app.use(cors({
+    origin: process.env.PUBLIC_FRONTEND_DOMAIN || "http://localhost:5173",
+    optionsSuccessStatus: 200
+}));
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to Course-Yuga");
 });
 
-app.use("/api/v1/user" , userRoute);
-app.use("/api/v1/course" , courseRoute);
-app.use("/api/v1/video" , videoRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/course", courseRoute);
+app.use("/api/v1/video", videoRoute);
 
-export default (req: VercelRequest, res: VercelResponse) => {
-    app(req, res);
-};
+async function startServer() {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log("Server started on port: " + PORT);
+        });
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        process.exit(1);
+    }
+}
+
+startServer();
+
+process.on("SIGINT", () => {
+    console.log("Server is shutting down...");
+    process.exit();
+});
+
+export default app;
