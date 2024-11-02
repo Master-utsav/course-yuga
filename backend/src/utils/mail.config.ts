29c22c4f3c258891f0bad8e5d8a05c1681dffb93
master-utsav <utsavjaiswal890@gmail.com>
2024-11-02 17:02:16 +0530

    fixes_cloudinary

--- backend/src/utils/mail.config.ts
+++ backend/src/utils/mail.config.ts
@@ -5,6 +5,7 @@ dotenv.config()
 export const transporter = nodemailer.createTransport({
     host: process.env.MAILER_HOST,
     port: 587,
+    secure: false,
     auth: {
       user: process.env.MAILER_USER,
       pass: process.env.MAILER_PASS,
